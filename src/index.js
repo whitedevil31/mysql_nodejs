7fba13bc4b2948097d92a0725eaa267321ba2c39
whitedevil31 <magz3116@gmail.com>
2020-12-25 15:18:13 +0530

    dotenv +

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
+const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+dotenv.config({ path: "../.env" });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const db = mysql.createConnection({
@@ -16,10 +18,9 @@ db.connect((err) => {
   console.log(" mysql connected");
 });
 
-// app.get("/", (req, res) => {
-//   res.send("hello working");
-//   console.log("hiiii");
-// });
+app.get("/", (req, res) => {
+  console.log("hiiii");
+});
 // app.get("/createdb", (req, res) => {
 //   let sql = "CREATE DATABASE testsqldatabase";
 //   db.query(sql, (err, result) => {
@@ -27,61 +28,62 @@ db.connect((err) => {
 //     res.send("database created");
 //     console.log(result);
 //   });
+//  });
+
+// app.get("/createtable", (req, res) => {
+//   let sql =
+//     "CREATE TABLE posts(id int AUTO_INCREMENT,title VARCHAR(255),body VARCHAR(255), PRIMARY KEY(id))";
+//   db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send("table created dude");
+//   });
 // });
 
-app.get("/createtable", (req, res) => {
-  let sql =
-    "CREATE TABLE posts(id int AUTO_INCREMENT,title VARCHAR(255),body VARCHAR(255), PRIMARY KEY(id))";
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send("table created dude");
-  });
-});
+// app.post("/postdata", (req, res) => {
+//   let data = { title: req.body.title, body: req.body.body };
 
-app.post("/postdata", (req, res) => {
-  let data = { title: req.body.title, body: req.body.body };
+//   let sql = "INSERT INTO posts SET ?";
+//   let query = db.query(sql, data, (err, result) => {
+//     if (err) throw err;
 
-  let sql = "INSERT INTO posts SET ?";
-  let query = db.query(sql, data, (err, result) => {
-    if (err) throw err;
-
-    res.status(201).send({ result });
-  });
-});
-app.get("/getdatas", (req, res) => {
-  let sql = "SELECT * from posts";
-  let query = db.query(sql, (err, results) => {
-    if (err) throw err;
-    console.log(results);
-    res.send("results showing");
-  });
-});
-app.get("/getsingledata/:id", (req, res) => {
-  let sql = `SELECT * from posts WHERE id=${req.params.id}`;
-  let query = db.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send({ result }).json();
-  });
-});
-app.get("/updatesinglepost/:id", (req, res) => {
-  let newupdate = "vit is pysc";
-  let sql = `UPDATE posts SET title='${newupdate}' WHERE id=${req.params.id}`;
-  let query = db.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send({ result }).json();
-  });
-});
-app.get("/deletesinglepost/:id", (req, res) => {
-  let sql = `DELETE FROM posts WHERE id=${req.params.id}`;
-  let query = db.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send("deleted da");
-  });
-});
-app.listen(3000, () => {
+//     res.status(201).send({ result });
+//   });
+// });
+// app.get("/getdatas", (req, res) => {
+//   let sql = "SELECT * from posts";
+//   let query = db.query(sql, (err, results) => {
+//     if (err) throw err;
+//     console.log(results);
+//     res.send("results showing");
+//   });
+// });
+// app.get("/getsingledata/:id", (req, res) => {
+//   let sql = `SELECT * from posts WHERE id=${req.params.id}`;
+//   let query = db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send({ result }).json();
+//   });
+// });
+// app.get("/updatesinglepost/:id", (req, res) => {
+//   let newupdate = "vit is pysc";
+//   let sql = `UPDATE posts SET title='${newupdate}' WHERE id=${req.params.id}`;
+//   let query = db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send({ result }).json();
+//   });
+// });
+// app.get("/deletesinglepost/:id", (req, res) => {
+//   let sql = `DELETE FROM posts WHERE id=${req.params.id}`;
+//   let query = db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send("deleted da");
+//   });
+// });
+port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log("server is up ");
 });
